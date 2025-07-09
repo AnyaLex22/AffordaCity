7b96f2d5a926df828ad51053e372cf2ff0ca4064
AnyaLex22 <alexandriaravenwolf@gmail.com>
2025-07-09 19:48:27 +0800

    .

--- backend/userCalc.js
+++ backend/userCalc.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const verifyToken = require('./auth');
-const Calculation = require('.server');
+const Calculation = require('./server');
 
 // GET /api/user-calculations
 router.get('/user-calculations', verifyToken, async (req, res) => {
